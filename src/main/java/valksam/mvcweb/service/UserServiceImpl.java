4128f9a674224081d49ab52d1b8a03f9ee1f3a46
ValkSam <do110473sva@gmail.com>
2016-01-17 23:59:26 +0200

    simple Servlet + Logging + postgress + hibenate + JUnit + SpringJdbc

--- src/main/java/valksam/mvcweb/service/UserServiceImpl.java
+++ src/main/java/valksam/mvcweb/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package valksam.mvcweb.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import valksam.mvcweb.LoggerWrapper;
 import valksam.mvcweb.model.User;
 import valksam.mvcweb.repository.UserRepository;
@@ -16,20 +18,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public User get(int id) {
         User user = userRepository.get(id);
         if (user == null) throw LOG.getNotFoundExeption("User with id = " + id + " not found !");
         return user;
     }
 
+    @Transactional
     public boolean delete(int id) {
         boolean result = userRepository.delete(id);
-        if (! result) throw LOG.getNotFoundExeption("User with id = " + id + " not found !");
+        if (!result) throw LOG.getNotFoundExeption("User with id = " + id + " not found !");
         return result;
     }
 
+    @Transactional
     public User save(User user) {
-        return userRepository.save(user);
+        User result = userRepository.save(user);
+        return result;
     }
 
 }
